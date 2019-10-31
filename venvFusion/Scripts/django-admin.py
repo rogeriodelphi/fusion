442951ed5e057c373c50b93d976d60f27386e04b
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-10-31 01:18:08 -0300

    first commit

---
+++ venvFusion/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrador\PycharmProjects\fusion\venvFusion\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
