5e3664e0b4f16bd77abf316441e34ea2e88ecb37
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-11-04 01:39:05 -0300

    Adicinando as páginas 404 e 500 ao Projeto

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import IndexView
-
+from .views import IndexView, TesteView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('teste/', TesteView.as_view(), name='teste'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = "Index.html"
+    template_name = 'Index.html'
+
+
+class TesteView(TemplateView):
+    template_name = '500.html'
