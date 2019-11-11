5d7a460afa20e817de297d097b775717c793f069
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-11-10 22:33:09 -0300

    Alterando o settings.py - conf. de e-mails

--- fusion/settings.py
+++ fusion/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email teste console
- EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 """
 # Email produção
